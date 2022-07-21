93fe866af036458bdff0511cead5887a43850200
hamza-007 <abdellaoui@essths.u-sousse.tn>
2022-07-21 16:52:29 +0100

    style(components) : fix styling issues

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,14 +1,12 @@
-import { Navigate, Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
-import { Home, Questions, Result, QuestionReview } from "../pages";
 
-const ProtectedRoute = ({children}) => {
-  const user = useAuth()
-  if (!user){
-    return <Navigate to="/login" />
+const ProtectedRoute = ({ children }) => {
+  const user = useAuth();
+  if (!user) {
+    return <Navigate to='/login' />;
   }
- 
+
   return children;
-   
 };
-export default ProtectedRoute
+export default ProtectedRoute;
